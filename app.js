9fd438846c81f342bad3512483085b3054a617dd
RETRADE-UK <general007store@gmail.com>
2026-09-14 17:14:02 +0100

    chore(cache): publish collapsed account sections v1.4.78

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
  */
 (function(){
   'use strict';
-  var v='20260914-v1477';
+  var v='20260914-v1478';
   var motionReady=false;
   var motionFallbackTimer=0;
 
@@ -70,6 +70,7 @@
       './item-account-adjustments.js',
       './partner-arrangements-v2.js',
       './partner-payment-allocations-v2.js',
+      './partner-collapse-defaults.js',
       './chart-polish.js',
       './chart-motion.js',
       './chart-finalize.js',
