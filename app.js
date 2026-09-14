5a03610126a029e0022cd0d2ea5491d2a7e323b5
RETRADE-UK <general007store@gmail.com>
2026-09-14 15:23:00 +0100

    feat(partners): load payment allocation workflow v1.4.76

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
  */
 (function(){
   'use strict';
-  var v='20260914-v1475';
+  var v='20260914-v1476';
   var motionReady=false;
   var motionFallbackTimer=0;
 
@@ -69,6 +69,7 @@
       './partner-account-cleanup.js',
       './item-account-adjustments.js',
       './partner-arrangements-v2.js',
+      './partner-payment-allocations.js',
       './chart-polish.js',
       './chart-motion.js',
       './chart-finalize.js',
