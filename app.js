fd316338c08a69c1e59a533ef7f5d595c1b3f0dd
RETRADE-UK <general007store@gmail.com>
2026-09-17 22:29:38 +0100

    chore(cache): load Sales skeleton truth mask v1.5.08

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
  */
 (function(){
   'use strict';
-  var v='20260917-v1507';
+  var v='20260917-v1508';
   window.__rtBuildId=v;
   var motionReady=false;
   var motionFallbackTimer=0;
@@ -106,6 +106,7 @@
       './partners-loading-dwell-v1505.js',
       './main-page-loading-motion-v1506.js',
       './main-page-truth-gate-v1507.js',
+      './sales-loading-mask-v1508.js',
       './document-exports.js',
       './chart-polish.js',
       './chart-motion.js',
