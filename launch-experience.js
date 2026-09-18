c763294a63c0d33a18deaf9d22448ce28f915402
RETRADE-UK <general007store@gmail.com>
2026-09-18 16:20:39 +0100

    chore(boot): align cold-start coordinator v1.5.25

--- launch-experience.js
+++ launch-experience.js
@@ -13,7 +13,7 @@
 (function(){
   'use strict';
 
-  var VERSION='20260918-v1524';
+  var VERSION='20260918-v1525';
   var root=document.documentElement;
   var t0=(window.performance&&performance.now)?performance.now():Date.now();
   var bodyObserver=null;
