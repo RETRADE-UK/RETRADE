3bc5c9d1dd7985cf1123b9bddf5e4f6dbdcc7d67
RETRADE-UK <general007store@gmail.com>
2026-09-18 20:37:10 +0100

    chore(boot): align Sales skeleton fix v1.5.29

--- launch-experience.js
+++ launch-experience.js
@@ -13,7 +13,7 @@
 (function(){
   'use strict';
 
-  var VERSION='20260918-v1528';
+  var VERSION='20260918-v1529';
   var root=document.documentElement;
   var t0=(window.performance&&performance.now)?performance.now():Date.now();
   var bodyObserver=null;
