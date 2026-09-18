500118162c0b2b8edd7b65ac6dea53d25845503f
RETRADE-UK <general007store@gmail.com>
2026-09-18 16:21:22 +0100

    chore(boot): align first-load polish v1.5.26

--- launch-experience.js
+++ launch-experience.js
@@ -13,7 +13,7 @@
 (function(){
   'use strict';
 
-  var VERSION='20260918-v1525';
+  var VERSION='20260918-v1526';
   var root=document.documentElement;
   var t0=(window.performance&&performance.now)?performance.now():Date.now();
   var bodyObserver=null;
